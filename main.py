ecf46895c92c4d2a2a52bacb178038ac8fe182a6
Mohsin Sarmad <mohsinsarmad.learn@gmail.com>
2025-08-07 14:36:35 +0500

    commit before grouping of the classes

---
+++ main.py
@@ -0,0 +1,32 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+from player import Player
+
+
+def main():
+    pygame.init
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    Clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    while pygame.init:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+        dt = Clock.tick(60) / 100
+        player.update(dt)
+
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
