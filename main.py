14d53db3110ce0d1675db25a5f3c44c3827390af
Mohsin Sarmad <mohsinsarmad.learn@gmail.com>
2025-08-07 15:00:35 +0500

    Added the groups

--- main.py
+++ main.py
@@ -10,22 +10,31 @@ def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     Clock = pygame.time.Clock()
-    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
     while pygame.init:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill("black")
-        player.draw(screen)
-        pygame.display.flip()
-        dt = Clock.tick(60) / 100
-        player.update(dt)
+        updatable.update(dt)
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        screen.fill("black")
+
+        for things in drawable:
+            things.draw(screen)
+
+        pygame.display.flip()
+
+        dt = Clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
